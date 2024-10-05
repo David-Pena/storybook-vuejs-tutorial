6b1be363025400f7208ea5812532ad084cbdbfdf
David-Pena <dev.davidpe@gmail.com>
2024-10-04 21:24:17 -0700

    feat: migrate Task.vue to script setup

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,3 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  task: {
+    type: Object,
+    required: true,
+    default: () => ({ id: '', state: '', title: '' }),
+    validator: (task) => ['id', 'state', 'title'].every((key) => key in task)
+  }
+})
+
+const emits = defineEmits(['archive-task', 'pin-task'])
+
+const classes = computed(() => ({
+  'list-item TASK_INBOX': props.task.state === 'TASK_INBOX',
+  'list-item TASK_PINNED': props.task.state === 'TASK_PINNED',
+  'list-item TASK_ARCHIVED': props.task.state === 'TASK_ARCHIVED'
+}))
+
+const isChecked = computed(() => props.task.state === 'TASK_ARCHIVED')
+
+const archiveTask = () => {
+  emits('archive-task', props.task.id)
+}
+
+const pinTask = () => {
+  emits('pin-task', props.task.id)
+}
+</script>
+
 <template>
   <div :class="classes">
     <label :for="'checked' + task.id" :aria-label="'archiveTask-' + task.id" class="checkbox">
@@ -31,48 +63,3 @@
     </button>
   </div>
 </template>
-
-<script>
-import { reactive, computed } from 'vue'
-
-export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Task',
-  props: {
-    task: {
-      type: Object,
-      required: true,
-      default: () => ({ id: '', state: '', title: '' }),
-      validator: (task) => ['id', 'state', 'title'].every((key) => key in task)
-    }
-  },
-  emits: ['archive-task', 'pin-task'],
-
-  setup(props, { emit }) {
-    props = reactive(props)
-    return {
-      classes: computed(() => ({
-        'list-item TASK_INBOX': props.task.state === 'TASK_INBOX',
-        'list-item TASK_PINNED': props.task.state === 'TASK_PINNED',
-        'list-item TASK_ARCHIVED': props.task.state === 'TASK_ARCHIVED'
-      })),
-      /**
-       * Computed property for checking the state of the task
-       */
-      isChecked: computed(() => props.task.state === 'TASK_ARCHIVED'),
-      /**
-       * Event handler for archiving tasks
-       */
-      archiveTask() {
-        emit('archive-task', props.task.id)
-      },
-      /**
-       * Event handler for pinning tasks
-       */
-      pinTask() {
-        emit('pin-task', props.task.id)
-      }
-    }
-  }
-}
-</script>
